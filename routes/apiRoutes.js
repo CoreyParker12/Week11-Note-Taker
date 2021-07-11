415b5fa936e7db95d9b6e861f533791581fb4d53
Corey Parker <coreyparker012@gmail.com>
2021-07-11 13:48:47 -0400

    Commenting code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,47 +1,38 @@
 // Require db.json
 const db = require('../db/db.json');
+
+// Require unique id generator
 const { v4: uuidv4 } = require('uuid');
 
-// ROUTES
+// GET, POST, and DELETE
 module.exports = (app) => {
 
   // Get the notes database from the db.json file
   app.get('/api/notes', (req, res) => res.json(db));
 
-  // app.get('/api/notes/:id', (req,res) => {
-  //   deleteNote = req.params.id;
-  //   console.log(deleteNote);
-
-  //   for (let i = 0; i < db.length; i++) {
-  //     if (deleteNote === db[i].id) {
-  //       return res.json(db[i]);
-  //     }
-  //   }
-
-  // })
-
-  // POST
-
+  // Posts the note to the application
   app.post('/api/notes', (req,res) => {
 
     note = req.body;
 
+    // Adds a unique id to the end of the object
     note.id = uuidv4();
-    console.log(note);
 
+    // Pushes the note
     db.push(note);
-
     res.json(true);
   });
 
   app.delete('/api/notes/:id', (req,res) => {
+    
+    // Grabs the id
     deleteNote = req.params.id;
 
+    // Logic to find the note associated with the id and deletes it
     for (let i = 0; i < db.length; i++) {
       if (deleteNote === db[i].id) {
         return res.json(db.splice(i, 1));
       }
     }
   });
-
-}
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
-// ROUTING
-
+// Routes to pages
 module.exports = (app) => {
 
+  // Notes page
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  // If no matching route is found default to home
+  // Default/home page
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
